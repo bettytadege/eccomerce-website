bb0699dc86e1f27983ecfddee07e1420847cebb4
bettytadege <bettytadege13@gmail.com>
2025-04-24 11:10:15 +0300

    refcator:add profile dashboatd route

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+
+import UserOrders from "./components/profile/UserOrders";
+import AllProducts from "./components/products/AllProducts";
+import EditProfile from "./components/profile/EditProfile";
+import ProductDetail from "./components/products/ProductDetail";
+import ConfirmOtp from "./components/confimOtp/ConfirmOtp";
+import ProfileDashboard from "./components/profile/ProfileDashboard";
+import Header from "./components/header/Header";
+import SignIn from "./components/signin/SignIn";
+import Signup from "./components/signup/Signup";
+import ForgotPassword from "./components/forgot password/ForgotPassword";
+import Home from "./page/Home";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Cart from "./components/cart/Cart";
+import { Toaster } from "sonner";
+
+const hiddenHeaderRoutes = [
+  "/signin",
+  "/signup",
+  "/confirmotp",
+  "/profiledashboard",
+  "/profiledashboard/editprofile",
+  "/forgotpassword",
+];
+
+function App() {
+  const location = useLocation();
+  const showHeader = !hiddenHeaderRoutes.includes(location.pathname);
+  return (
+    <>
+      {showHeader && <Header />}
+
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<SignIn />} path="/signin" />
+        <Route element={<Signup />} path="/signup" />
+        <Route element={<ConfirmOtp />} path="/confirmotp" />
+        <Route element={<ForgotPassword />} path="/forgotpassword" />
+        {/* nested route */}
+        <Route element={<ProfileDashboard />} path="/profiledashboard">
+          <Route element={<UserOrders />} index />
+          <Route element={<EditProfile />} path="editprofile" />
+        </Route>
+        {/* nested route */}
+        <Route element={<ProductDetail />} path="/productdetail" />
+        <Route element={<Cart />} path="/cart" />
+        <Route element={<AllProducts />} path="/allproducts" />
+      </Routes>
+      <Toaster richColors position="top-center" />
+    </>
+  );
+}
+
+export default App;
