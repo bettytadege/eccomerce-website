cf7ac4539d40917b1ed7bced42624a435781eb9a
bettytadege <bettytadege13@gmail.com>
2025-04-30 10:47:03 +0300

    feat:add view all button

---
+++ src/components/common/ViewAllButton.tsx
@@ -0,0 +1,13 @@
+
+
+function ViewAllButton() {
+  return (
+    <div className="">
+        <button className='bg-white text font-light text-black border border-black py-3 px-8  hover:outline-[1.4px] outline-black rounded-sm shadow-none'>
+            View all
+        </button>
+    </div>
+  )
+}
+
+export default ViewAllButton
