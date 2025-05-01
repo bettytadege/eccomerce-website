be6e9f6a078c65702c4d12a069962ce1031903fa
bettytadege <bettytadege13@gmail.com>
2025-05-01 13:25:14 +0300

    feat:add resuable section divider

---
+++ src/components/common/SectionDivider.tsx
@@ -0,0 +1,13 @@
+
+
+function SectionDivider() {
+  return (
+    <div className="flex items-center gap-4 justify-center">
+    <div className="border w-60"></div>
+    <span>or</span>
+    <div className="border w-60"></div>
+  </div>
+  )
+}
+
+export default SectionDivider
