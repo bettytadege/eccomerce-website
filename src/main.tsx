55f42dafbaf6870ddea610482daa0fecb646dfbb
bettytadege <bettytadege13@gmail.com>
2025-05-02 12:06:21 +0300

    feat:wrap main with cart provider

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,13 @@ import "@fontsource/roboto";
 import "@fontsource/inter"; 
 import "@fontsource/condiment"
 import { AuthProvider } from './components/context/AuthContext.tsx';
+import { CartProvider } from './components/context/CartContext.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
   <AuthProvider>
+    <CartProvider>
     <App />
+    </CartProvider>
     </AuthProvider>
   </BrowserRouter>,
 )
