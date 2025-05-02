9c4ea636e9078df6df242fa80967bd76c9b0adcc
bettytadege <bettytadege13@gmail.com>
2025-05-02 12:07:02 +0300

    feat:home page added

---
+++ src/page/Home.tsx
@@ -0,0 +1,19 @@
+import { CarouselPlugin } from "@/components/carousel/CarouselPlugin";
+import Product from "@/components/products/Product";
+
+
+function Home() {
+  return (
+    <>
+      <div className="w-full ">
+        <div className="space-y-10">
+          <CarouselPlugin />
+          <Product />
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
