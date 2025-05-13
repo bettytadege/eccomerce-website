a9f38bcf537ac9474ba2db95f428cda500b5d910
bettytadege <bettytadege13@gmail.com>
2025-05-13 16:28:07 +0300

    feat:add dummy data for carousel plugin

---
+++ src/data/data.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: "p001",
+    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
+    name: " Jacket Women Long Sleeve",
+    price: 25.99,
+  },
+  {
+    id: "p002",
+    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
+    name: "Jacket Women Long Sleeve",
+    price: 89.99,
+  },
+  {
+    id: "p003",
+    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
+    name: "Jacket Women Long Sleeve",
+    price: 59.99,
+  },
+  {
+    id: "p004",
+    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
+    name: "Jacket Women Long Sleeve",
+    price: 29.5,
+  },
+  {
+    id: "p004",
+    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
+    name: "Jacket Women Long Sleeve",
+    price: 29.5,
+  },
+  {
+    id: "p004",
+    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
+    name: "Jacket Women Long Sleeve",
+    price: 29.5,
+  },
+];
