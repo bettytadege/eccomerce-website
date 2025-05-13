58c42ee9cb5128d0d040d5146f0b9c2a7dc86e8f
bettytadege <bettytadege13@gmail.com>
2025-05-13 16:27:28 +0300

    feat:add messgae icon at the bar

---
+++ src/components/header/Message.tsx
@@ -0,0 +1,16 @@
+
+import { PiChatTextThin } from 'react-icons/pi'
+
+function Message() {
+  return (
+    <>
+    <div className="">
+        <a href="">
+        <PiChatTextThin  size={25} color='black' />
+        </a>
+    </div>
+    </>
+  )
+}
+
+export default Message
