63c63ca83721d4b1737d8b0567401c746ad57d69
bettytadege <bettytadege13@gmail.com>
2025-06-17 12:13:51 +0300

    refcator:update base url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const instance=axios.create({
-    baseURL:'http://localhost:7880/',
+    // baseURL:'http://localhost:7880/',
+    baseURL:'https://ecommerce-backend-07hy.onrender.com/',
    
         headers: { "Content-Type": "application/json" },
        
