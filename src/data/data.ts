53ffe6f67617a49bc5398b60d8b6f3e7eb6cd8d6
bettytadege <bettytadege13@gmail.com>
2025-06-17 12:58:45 +0300

    refcator:upade description and image

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,38 +1,38 @@
-export const products = [
-  {
-    id: "p001",
-    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
-    name: " Jacket Women Long Sleeve",
-    price: 25.99,
-  },
-  {
-    id: "p002",
-    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
-    name: "Jacket Women Long Sleeve",
-    price: 89.99,
-  },
+import img1 from '../assets/images/shoes coll 1.jpg';
+import img4 from '../assets/images/bag2.jpg';
+import puffer from '../assets/images/puffer2.jpg'
+import hoodie from '../assets/images/hoodie_cleanup.jpg'
+
+
+
+
+
+export const collection = [
+ {
+  id: "p001",
+  img: img1,
+  name: "New Classic Everyday Shoes",
+  description: "Comfortable and versatile shoes designed for daily wear with a clean, classic look.",
+},
+{
+  id: "p002",
+  img: img4,
+  name: "Classic Everyday Backpack",
+  description: "A reliable backpack with a simple design, ideal for everyday use and light travel.",
+},
+
   {
     id: "p003",
-    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
-    name: "Jacket Women Long Sleeve",
-    price: 59.99,
+    img: puffer,
+    name: "Winter Puffer Coat ",
+
   },
+ 
+ 
   {
-    id: "p004",
-    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
-    name: "Jacket Women Long Sleeve",
-    price: 29.5,
-  },
-  {
-    id: "p004",
-    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
-    name: "Jacket Women Long Sleeve",
-    price: 29.5,
-  },
-  {
-    id: "p004",
-    img: "https://ae-pic-a1.aliexpress-media.com/kf/A08f830c1301a45649879159a806293d90.jpg_960x960q75.jpg_.avif",
-    name: "Jacket Women Long Sleeve",
-    price: 29.5,
+    id: "p006",
+    img: hoodie,
+    name: "Men's  Hoodie Cozy All-Day Wear",
+    
   },
 ];
