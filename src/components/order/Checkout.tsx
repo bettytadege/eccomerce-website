c00d6f8d10227e7cc6023089ff2bc64e63db1dce
bettytadege <bettytadege13@gmail.com>
2025-06-17 12:26:34 +0300

    feat:add checkout component

---
+++ src/components/order/Checkout.tsx
@@ -0,0 +1,57 @@
+
+import { instance } from '@/api/axiosInstance'
+import Summary from '../common/Summary'
+import AddressSection from './AddressSection'
+
+import OrderItemDetail from './OrderItemDetail'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+
+
+
+function Checkout() {
+  // const [payment,setPayment]=useState()
+  const [order,setOrder]=useState([])
+   const{userId,}=useParams<string>()
+
+  //calculate total
+   const total = order?.reduce((sum, item) => {
+  return sum + (item?.subTotal );
+}, 0) ?? 0;
+
+
+
+
+  const fetchOrder=async(id:string)=>{
+    const res=await instance.get(`order?userId=${id}`)
+    console.log('order',res)
+    setOrder(res.data)
+  }
+  useEffect(()=>{if(userId)fetchOrder(userId)},[userId])
+
+
+  const handlePayment=async()=>{
+    console.log('payment')
+    const pay=await instance.post('initialize')
+  }
+  return (
+    <>
+    <div className="bg-[#F5F5F5] w-full min-h-screen p-10">
+    
+    <div className="flex gap-10 w-full">
+    <div className="space-y-3 w-full">
+  
+    <AddressSection/>
+    <OrderItemDetail order={order}/>
+    {/* <PaymentMethod/> */}
+    </div>
+        <Summary name={'poceed to pay'} onClick={handlePayment} subTotal={total} item={order?.length}/>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Checkout
