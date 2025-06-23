98e0bb3f59c3304482a3bf945b0ccb2f526c79dd
bettytadege <bettytadege13@gmail.com>
2025-06-23 12:44:06 +0300

    refcator:add motion (animation)on the card

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import { motion } from "framer-motion";
+
 
 type ProductItemProps = {
   id: string;
@@ -11,35 +14,56 @@ type ProductItemProps = {
 function ProductItem({ img, price, name, id }: ProductItemProps) {
   const navigate = useNavigate();
   const { setProductId } = useAuth();
+
+  
+
   const handleClick = (id: string) => {
     setProductId(id);
     navigate(`/product/${id}`);
   };
-  return (
-    <>
-     
-      <div
-        className="bg-white w-[190px] h-[270px] sm:w-[260px] sm:h[400px] md:w-[360px] md:h-[410px] lg:w-[263px]  lg:h-[345px]"
-        onClick={() => handleClick(id)}
-      >
-        {"  "}
+const cardVariants = {
+  hidden: {
+    x: 0,
+    y: 30,         
+    opacity: 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "tween",
+      delay: 0.1,
+      duration: 0.5,
+      ease: "easeOut",
+    }as any,
+  },
+};
 
-        <div className="w-full h-[200px]   md:h-[300px] sm:h-[220px] lg:h-[230px]  ">
-          <img
-            src={img}
-            alt=""
-            className="w-full h-full rounded-2xl object-cover"
-          />
-        </div>
-        <div className="text  pt-1 flex flex-col gap-1 ">
-          <a href="" className="hover:underline font-light line-clamp-1 ">
-            {name}
-          </a>
-          <p className="font-normal">{price} ETB</p>
-        </div>
+
+  return (
+    <motion.div
+      whileInView={'show'}
+      initial='hidden'
+     variants={cardVariants}
+      viewport={{once:true}}
+      className="bg-white w-[190px] h-[270px] sm:w-[260px] sm:h[400px] md:w-[360px] md:h-[410px] lg:w-[263px]  lg:h-[345px]"
+      onClick={() => handleClick(id)}
+    >
+      <div className="w-full h-[200px]   md:h-[300px] sm:h-[220px] lg:h-[230px]  ">
+        <img
+          src={img}
+          alt=""
+          className="w-full h-full rounded-2xl object-cover"
+        />
       </div>
-     
-    </>
+      <div className="text  pt-1 flex flex-col gap-1 ">
+        <a href="" className="hover:underline font-light line-clamp-1 ">
+          {name}
+        </a>
+        <p className="font-normal">{price} ETB</p>
+      </div>
+    </motion.div>
   );
 }
 
