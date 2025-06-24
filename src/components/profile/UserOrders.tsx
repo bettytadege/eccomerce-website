b03a30a72ac251ba63cf8b4f436fe20ab7cbbe39
bettytadege <bettytadege13@gmail.com>
2025-06-24 09:12:49 +0300

    refactor:make responsive

--- src/components/profile/UserOrders.tsx
+++ src/components/profile/UserOrders.tsx
@@ -1,19 +1,16 @@
 import { Card } from "../ui/card"
 
-
 function UserOrders() {
   return (
-   <>
-   <div className="mx-36 ">
-    <h1 className="font-semibold text-lg py-10">Orders</h1>
-    <Card className="shadow-none border-0 h-28">
-      <div className="flex flex-col justify-center items-center gap-2 py-4  text-sm">
-        <p className="font-semibold">No orders yet</p>
-        <span>Go to store to place an order</span>
-      </div>
-    </Card>
-   </div>
-   </>
+    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <h1 className="font-semibold text-lg py-10">Orders</h1>
+      <Card className="shadow-none border-0 h-28">
+        <div className="flex flex-col justify-center items-center gap-2 py-4 text-sm text-center">
+          <p className="font-semibold">No orders yet</p>
+          <span>Go to store to place an order</span>
+        </div>
+      </Card>
+    </div>
   )
 }
 
