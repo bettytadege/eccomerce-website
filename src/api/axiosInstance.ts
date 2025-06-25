55ad01c1b8d1f2731a9d92a712906306b301419c
bettytadege <bettytadege13@gmail.com>
2025-06-25 12:21:03 +0300

    refcator:chnage base url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance=axios.create({
-    baseURL:'http://localhost:7880/',
-    // baseURL:'https://ecommerce-backend-07hy.onrender.com/',
+    // baseURL:'http://localhost:7880/',
+    baseURL:'https://ecommerce-backend-07hy.onrender.com/',
    
         headers: { "Content-Type": "application/json" },
        
