491dffc610ef41dd14e8a2e7c2d2455a87fbe3fa
bettytadege <bettytadege13@gmail.com>
2025-06-25 12:20:06 +0300

    refcator:make the banner responsive

--- src/components/new arrival/Banner.tsx
+++ src/components/new arrival/Banner.tsx
@@ -11,7 +11,7 @@ import { Card, CardContent } from "../ui/card"
 
 const images = [
   "https://s.alicdn.com/@img/imgextra/i2/O1CN01ozxKbu1c1N7DpGDHT_!!6000000003540-2-tps-760-608.png",
-  "https://pixelixe.com/blog/images/250/e-commerce-banner-strategy.jpg",
+
   "https://s.alicdn.com/@img/imgextra/i3/O1CN01ghTrQo1VeZhwJZBjr_!!6000000002678-2-tps-760-608.png",
 ]
 
@@ -21,23 +21,22 @@ function Banner() {
   )
 
   return (
-    <div className=" border w-[35%] h-[320px] overflow-hidden">
+    <div className="w-full hidden lg:block max-w-[420px] h-auto overflow-hidden"> {/* Adjusted wrapper */}
       <Carousel
         plugins={[plugin.current]}
         onMouseEnter={plugin.current.stop}
         onMouseLeave={plugin.current.reset}
-        className="h-full"
+        className="w-full h-full"
       >
-        <CarouselContent>
+        <CarouselContent className="w-full h-full">
           {images.map((src, index) => (
-            <CarouselItem key={index} className="h-full">
-              <Card className="rounded-none border-none shadow-none h-full">
+            <CarouselItem key={index} className="h-full w-full">
+              <Card className="rounded-none border-none shadow-none h-full w-full">
                 <CardContent className="p-0 h-full w-full">
-                  
                   <img
                     src={src}
                     alt={`Banner ${index + 1}`}
-                    className="w-full h-full object-cover transition-all duration-500"
+                    className="w-full h-full object-cover rounded"
                   />
                 </CardContent>
               </Card>
